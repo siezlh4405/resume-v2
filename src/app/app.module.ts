2479a27bf29499e869886505590a6d9fcd518800
henry.shih <siezlh4405@outlook.com>
2023-08-31 00:37:19 +0800

    feat: add swiper & school

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ProfileComponent } from './resume/components/profile/profile.component'
 import { AboutMeComponent } from './resume/components/about-me/about-me.component';
 import { SkillComponent } from './resume/components/skill/skill.component';
 import { ExperiencesComponent } from './resume/components/experiences/experiences.component';
+import { SideProjectComponent } from './resume/components/side-project/side-project.component';
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { ExperiencesComponent } from './resume/components/experiences/experience
     ProfileComponent,
     AboutMeComponent,
     SkillComponent,
-    ExperiencesComponent
+    ExperiencesComponent,
+    SideProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
